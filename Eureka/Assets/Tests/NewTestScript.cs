00d16f1f6eaab243613c94782ca439681fb05385
Noa <boxship.noah@gmail.com>
2023-06-07 21:27:37 +0900

    update for reconstruct folder

---
+++ Eureka/Assets/Tests/NewTestScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewTestScript {
+
+    [Test]
+    public void ExampleTest()
+    {
+        var a = 10;
+        var b = 10;
+        Assert.That(a == b);
+    }
+
+    [UnityTest]
+    public IEnumerator ExampleTestEnumerator()
+    {
+        Assert.That(1 < 10);
+        yield return null;
+        Assert.That(2 < 10);
+        yield return null;
+        Assert.That(3 < 10);
+    }
+
+}
